7e04f92377f5f47c317787f7dd3f33f15a2e5f65
Daniel Silva <daniel24maio@gmail.com>
2022-07-24 17:41:01 -0300

    Aula 8: encode e decode, utf8

--- arquivos-para-csv.php
+++ arquivos-para-csv.php
@@ -6,12 +6,12 @@ $outrosCursos = file('cursos-php.txt');
 $arquivoCsv = fopen('cursos.csv','w');
 
 foreach ($meusCurso as $curso){
-    $linha = [trim($curso), 'Sim'];
+    $linha = [trim(utf8_decode($curso)), 'Sim'];
     fputcsv($arquivoCsv,$linha,';');
 }
 
 foreach ($outrosCursos as $curso){
-    $linha = [trim($curso), 'Não'];
+    $linha = [trim(utf8_decode($curso)), utf8_decode('Não')];
     fputcsv($arquivoCsv,$linha,';');
 }
 
