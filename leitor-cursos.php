2927ad6004a7b538d9306d0dc62daabbdb13ce65
Daniel Silva <daniel24maio@gmail.com>
2022-07-24 14:20:10 -0300

    Aula-2: Lendo arquivo de acordo com o tamanho, filesize.

--- leitor-cursos.php
+++ leitor-cursos.php
@@ -1,7 +1,9 @@
 <?php
 $arquivo = fopen('lista-cursos.txt', 'r');
 
-while (!feof($arquivo)) {
-    $curso = fgets($arquivo);
-    echo $curso;
-}
+$tamanhoDoArquivo = filesize('lista-cursos.txt');
+$cursos = fread($arquivo, $tamanhoDoArquivo);
+
+echo $cursos;
+
+fclose($arquivo);
